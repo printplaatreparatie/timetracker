bd6f4d7727702305ad4c2b50d70efc412cab2042
printplaatreparatie <60609704+printplaatreparatie@users.noreply.github.com>
2026-09-08 14:30:01 +0200

    Remove autoload requirement to stop log flood
    Quick-n-dirty fix for #271

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -2,7 +2,7 @@
 
 namespace OCA\TimeTracker\AppInfo;
 
-require_once __DIR__ . '/../../vendor/autoload.php';
+# require_once __DIR__ . '/../../vendor/autoload.php';
 
 use \OCP\AppFramework\App;
 use \OCP\AppFramework\IAppContainer;
